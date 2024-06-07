9e6962b440fff901d23a0b32cf625b42308a2e53
guoj5 <guoj5@VUSELNX-3BBGRN3.vuds.vanderbilt.edu>
2024-06-07 14:03:26 -0500

    Initial commit

---
+++ bash/run_experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the path to the script and the common arguments
+SCRIPT_PATH="/home/guoj5/Desktop/cellvit/CellViT-kidney/cell_segmentation/run_cellvit_mod.py"
+GPU="0"
+
+# Define the configuration files for each experiment 
+CONFIG1="/home/guoj5/Desktop/cellvit/CellViT-kidney/configs/examples/cell_segmentation/my_experiment_logs/train_cellvit/train_fold4.yaml"
+#CONFIG2="/home/guoj5/Desktop/cellvit/CellViT-kidney/configs/examples/cell_segmentation/my_experiment_logs/train_cellvit/train_fold3.yaml"
+
+# Run the first experiment
+python $SCRIPT_PATH --gpu $GPU --config $CONFIG1
+
+# Run the second experiment
+#python $SCRIPT_PATH --gpu $GPU --config $CONFIG2
